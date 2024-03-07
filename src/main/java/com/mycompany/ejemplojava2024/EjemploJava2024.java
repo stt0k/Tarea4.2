2e2cfec2add8f621a6425a7a5baa198b9403a07c
stt0k <110210725+stt0k@users.noreply.github.com>
2024-03-07 18:05:14 +0000

    Update EjemploJava2024.java

--- src/main/java/com/mycompany/ejemplojava2024/EjemploJava2024.java
+++ src/main/java/com/mycompany/ejemplojava2024/EjemploJava2024.java
@@ -11,6 +11,6 @@ package com.mycompany.ejemplojava2024;
 public class EjemploJava2024 {
 
     public static void main(String[] args) {
-        System.out.println("Hola Ivan Vazquez");
+        System.out.println("Hola Ivan Vazquez, buenas tardes");
     }
 }

eb8ee4b98bcce7f9bf6be1268515a2d645eca709
Stok <Stok@Revision-PC>
2024-03-07 17:21:06 +0000

    Version 1 del proyecto

---
+++ src/main/java/com/mycompany/ejemplojava2024/EjemploJava2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplojava2024;
+
+/**
+ *
+ * @author Stok
+ */
+public class EjemploJava2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Ivan Vazquez");
+    }
+}
